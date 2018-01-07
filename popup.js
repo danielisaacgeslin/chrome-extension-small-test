ff076b2f905f657c7a02513571cd33b40635cd75
Daniel Isaac Geslin <danielisaacgeslin@gmail.com>
2018-01-07 21:09:54 +0100

    some stuff

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
-function interact() {
-  console.log('interacting');
+function tabFunction() {
+  console.log('tabFunction :)')
+  console.log(document.querySelectorAll('div'));
 }
+
+document.getElementById('interact').addEventListener('click', () => {
+  chrome.tabs.executeScript({ code: `(${tabFunction.toString()})()` })
+});
